e9bbe7950cf27b6d75e3bbf0259a9bac13c26147
Elzbieta <elzbieta_warzecha@wp.pl>
2023-04-15 13:41:25 +0200

    Additional functions for participants

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import com.company.enroller.model.Participant;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,37 +11,52 @@ import java.util.Collection;
 @Component("participantService")
 public class ParticipantService {
 
-	DatabaseConnector connector;
+    DatabaseConnector connector;
 
-	public ParticipantService() {
-		connector = DatabaseConnector.getInstance();
-	}
+    public ParticipantService() {
+        connector = DatabaseConnector.getInstance();
+    }
 
-	public Collection<Participant> getAll() {
-		return connector.getSession().createCriteria(Participant.class).list();
-	}
+    public Collection<Participant> getAll() {
+        return connector.getSession().createCriteria(Participant.class).list();
+    }
 
-	public Participant findByLogin(String login) {
-		return connector.getSession().get(Participant.class, login);
-	}
+    public Collection<Participant> getAll(String sortBy, String sortOrder, String key) {
+        String hql = "FROM Participant";
+        if (!key.equals("")) {
+            hql += " WHERE login like '%" + key + "%'";
+        }
+        if (sortBy.equals("login")) {
+            hql += " ORDER BY login";
+            if (sortOrder.equals("ASC") || sortOrder.equals("DESC")) {
+                hql += " " + sortOrder;
+            }
+        }
+        Query query = connector.getSession().createQuery(hql);
+        return query.list();
+    }
 
-	public Participant add(Participant participant) {
-		Transaction transaction = connector.getSession().beginTransaction();
-		connector.getSession().save(participant);
-		transaction.commit();
-		return participant;
-	}
+    public Participant findByLogin(String login) {
+        return connector.getSession().get(Participant.class, login);
+    }
 
-	public void update(Participant participant) {
-		Transaction transaction = connector.getSession().beginTransaction();
-		connector.getSession().merge(participant);
-		transaction.commit();
-	}
+    public Participant add(Participant participant) {
+        Transaction transaction = connector.getSession().beginTransaction();
+        connector.getSession().save(participant);
+        transaction.commit();
+        return participant;
+    }
 
-	public void delete(Participant participant) {
-		Transaction transaction = connector.getSession().beginTransaction();
-		connector.getSession().delete(participant);
-		transaction.commit();
-	}
+    public void update(Participant participant) {
+        Transaction transaction = connector.getSession().beginTransaction();
+        connector.getSession().merge(participant);
+        transaction.commit();
+    }
+
+    public void delete(Participant participant) {
+        Transaction transaction = connector.getSession().beginTransaction();
+        connector.getSession().delete(participant);
+        transaction.commit();
+    }
 
 }
